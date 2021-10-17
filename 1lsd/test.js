7b73f9a98216f9faa7ed2bd48067337bf8b8b2e9
udexon <udexon@users.noreply.github.com>
2021-10-17 10:05:07 +0800

    Update test.js

--- 1lsd/test.js
+++ 1lsd/test.js
@@ -2,4 +2,4 @@
 // u_ngrok = 'https://add5-113-211-144-109.ngrok.io'
 
 console.log( '2021 10 17' )
-u_ngrok = 'http://7f45-2001-d08-1b97-9de7-6dee-d496-6ae5-4e83.ngrok.io'
+u_ngrok = 'https://7f45-2001-d08-1b97-9de7-6dee-d496-6ae5-4e83.ngrok.io'
