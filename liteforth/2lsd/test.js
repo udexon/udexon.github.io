7c2a1ad48d71dcd801e5931ac165d0ab6be13021
udexon <udexon@users.noreply.github.com>
2021-10-24 01:30:38 +0800

    Add files via upload

---
+++ liteforth/2lsd/test.js
@@ -0,0 +1,8 @@
+// console.log( '2021 10 10' )
+// u_ngrok = 'https://add5-113-211-144-109.ngrok.io'
+
+// console.log( '2021 10 17' )
+// u_ngrok = 'https://7f45-2001-d08-1b97-9de7-6dee-d496-6ae5-4e83.ngrok.io'
+
+console.log( '2021 10 20' )
+u_ngrok = 'https://1491-2001-d08-228b-3f97-9af1-2dd6-324a-ee51.ngrok.io'
