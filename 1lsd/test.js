50f8916b03c7982140b16e295d4df88cfd130920
udexon <udexon@users.noreply.github.com>
2021-10-29 10:01:03 +0800

    Update test.js

--- 1lsd/test.js
+++ 1lsd/test.js
@@ -7,5 +7,9 @@
 // console.log( '2021 10 20' )
 // u_ngrok = 'https://1491-2001-d08-228b-3f97-9af1-2dd6-324a-ee51.ngrok.io'
 
-console.log( '2021 10 25' )
-u_ngrok = 'https://5cee-2001-d08-2280-70ae-7167-13f4-a9e1-4e4d.ngrok.io'
+// console.log( '2021 10 25' )
+// u_ngrok = 'https://5cee-2001-d08-2280-70ae-7167-13f4-a9e1-4e4d.ngrok.io'
+
+console.log( '2021 10 29' )
+u_ngrok = 'https://ea39-113-211-144-235.ngrok.io'
+
