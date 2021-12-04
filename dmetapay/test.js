e0ac51f2e03d19949c87c6dce9b7512248ac3c3d
udexon <udexon@users.noreply.github.com>
2021-12-04 16:24:22 +0800

    Update test.js

--- dmetapay/test.js
+++ dmetapay/test.js
@@ -13,5 +13,8 @@
 // console.log( '2021 10 29' )
 // u_ngrok = 'https://ea39-113-211-144-235.ngrok.io'
 
-console.log( '2021 11 13' )
+// console.log( '2021 11 13' )
+// u_ngrok = 'https://308d-2001-d08-1b9c-ff46-65b6-10c5-7282-981a.ngrok.io'
+
+console.log( '2021 12 04' )
 u_ngrok = 'https://308d-2001-d08-1b9c-ff46-65b6-10c5-7282-981a.ngrok.io'
